d1882e1a9d1393b243fdeeef1891292b9e3fed7e
hyu-resilink <boctulanm@gmail.com>
2026-04-14 12:34:58 +0800

    notif please work

--- api/notify.js
+++ api/notify.js
@@ -9,7 +9,10 @@ if (!getApps().length) {
     credential: cert({
       projectId:   process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey:  process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY
+  ?.replace(/^"/, "")      // remove leading quote if present
+  .replace(/"$/, "")       // remove trailing quote if present  
+  .replace(/\\n/g, "\n"),
     }),
   });
 }
